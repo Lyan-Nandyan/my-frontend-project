c9edac0d9a395cefbd2929fcb5a7ae8beac89c7f
Lyan_Nandyan <lyannandyan@gmail.com>
2024-11-19 17:21:03 +0700

    lupa import ternyata

--- src/index.jsx
+++ src/index.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'; // Import file CSS dengan Tailwind;
 import Login from './pages/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Register from './pages/Register';
 import ImageGallery from './pages/Dashboard';
 // Definisikan routing
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router>
+  <BrowserRouter>
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/regis" element={<Register />} />
       <Route path="/dashboard" element={<ImageGallery />} />
     </Routes>
-  </Router>
+  </BrowserRouter>
 );
